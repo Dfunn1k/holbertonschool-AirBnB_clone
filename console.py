7610d89b2b1f8c920b3e8b2960257f845d01a459
BigDany1792 <dany1792dota@gmail.com>
2022-06-28 18:21:19 -0500

    ci: added console v1.01

---
+++ console.py
@@ -0,0 +1,34 @@
+import cmd
+"""
+User this module for console
+
+(HBNB CONSOLE)
+"""
+
+
+class HBNBCommand(cmd.Cmd):
+    """Console for instances that inherit"""
+    prompt = '(hbnb)'
+    
+    def do_EOF(self, line):
+        """Terminates the running program"""
+        return True
+
+    def help_EOF(self):
+        return
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def help_quit(self):
+        print("Quit command to exit the program\n")
+
+    def emptyline(self):
+        """Ignore empty lines"""
+        return cmd.Cmd.emptyline(self)
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
