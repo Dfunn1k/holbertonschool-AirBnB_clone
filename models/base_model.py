0bdfd85ac25fe33d8c778e93d6c3f6004e0b5baa
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-06-28 23:29:40 -0500

    v1 of task 3 and task 4

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+import uuid
+from datetime import datetime
+
+""""""
+
+
+class BaseModel():
+    """"""
+    def __init__(self, *args, **kwargs):
+        """"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            new_dict = {}
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    new_dict[key] = datetime.fromisoformat(value)
+                elif key != '__class__':
+                    new_dict[key] = value
+            self.__dict__.update(new_dict)
+
+    def __str__(self):
+        """"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """"""
+        instance_dict = self.__dict__
+        new_dict = {}
+        new_dict["__class__"] = type(self).__name__
+        for key, value in instance_dict.items():
+            if key == 'created_at' or key == 'updated_at':
+                new_dict[key] = value.isoformat()
+            else:
+                new_dict[key] = value
+
+        return new_dict
