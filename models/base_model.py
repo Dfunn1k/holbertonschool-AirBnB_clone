09aa59ed9817359236dd3dca800b30fbc448a3b2
BigDany1792 <dany1792dota@gmail.com>
2022-06-29 12:20:08 -0500

    update: base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,8 @@
-import uuid
 from datetime import datetime
+import uuid
+import models
+
+
 """"""
 
 
@@ -9,8 +12,10 @@ class BaseModel():
         """"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            models.storage.new(self)
+
         else:
             new_dict = {}
             for key, value in kwargs.items():
@@ -27,6 +32,7 @@ class BaseModel():
     def save(self):
         """"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """"""
@@ -39,9 +45,12 @@ class BaseModel():
             else:
                 new_dict[key] = value
 
-<<<<<<< HEAD
-identif = uuid.uuid4()
-print(type(identif))
-=======
         return new_dict
->>>>>>> f1994926428d136bdf29e5bc9a95e5b34a149068
+
+
+all_objs = models.storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
+
