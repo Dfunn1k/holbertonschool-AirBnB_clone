4b30322c1ec72927ad9f00ff2cad74b7ccf57e5b
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-06-29 18:53:10 -0500

    Fix relative path in models/__init__.py file

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """"""
-from models.engine.file_storage import FileStorage
+from .engine import file_storage
 
-
-storage = FileStorage()
+storage = file_storage.FileStorage()
 storage.reload()
