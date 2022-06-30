60f07bae634c97cc7bdcb5db1e6b5caba7297f09
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-06-30 17:28:24 -0500

    Add docstring to user model file

--- models/user.py
+++ models/user.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""""""
+"""User Class implementation"""
 from .base_model import BaseModel
 
 
 class User(BaseModel):
+    """User class that inherits from BaseModel"""
     email = ""
     password = ""
     first_name = ""
