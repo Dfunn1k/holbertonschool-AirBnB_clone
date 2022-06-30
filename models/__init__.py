9f247c82c181aff4ae0a5356a49a65fca0b68cc6
BigDany1792 <dany1792dota@gmail.com>
2022-06-30 06:39:03 -0500

    ci: added pycache

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """"""
 from .engine import file_storage
+from models.base_model import BaseModel
+
+dict_class = {
+    "BaseModel": BaseModel
+}
 
 storage = file_storage.FileStorage()
 storage.reload()
