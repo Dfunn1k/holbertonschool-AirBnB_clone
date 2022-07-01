669b5b32ad4a877c2199da986e0a26b665a9ef04
BigDany1792 <dany1792dota@gmail.com>
2022-06-30 19:56:48 -0500

    ci: added new classes for package

--- models/__init__.py
+++ models/__init__.py
@@ -3,11 +3,22 @@
 from .engine import file_storage
 from .base_model import BaseModel
 from .user import User
+from .state import State
+from .city import City
+from .amenity import Amenity
+from .place import Place
+from .review import Review
 
 dict_class = {
     "BaseModel": BaseModel,
-    "User": User
+    "User": User,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Place": Place,
+    "Review": Review
 }
+
 
 storage = file_storage.FileStorage()
 storage.reload()
