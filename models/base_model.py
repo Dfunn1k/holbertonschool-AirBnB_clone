5a44905d83cb7fa786d8c80e37c41f33db4ad1b6
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-07-02 10:56:01 -0500

    Add docstrings

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""""""
+"""BaseModel Class implementation"""
 from datetime import datetime
 import uuid
 import models
 
 
 class BaseModel():
-    """"""
+    """BaseModel class"""
     def __init__(self, *args, **kwargs):
-        """"""
+        """Initial instance"""
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.updated_at = datetime.now()
@@ -25,16 +25,16 @@ class BaseModel():
             self.__dict__.update(new_dict)
 
     def __str__(self):
-        """"""
+        """String representation of the instance"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """"""
+        """Save a dict representation of the instance into a file"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """"""
+        """Convert the instance to a dict representation"""
         instance_dict = self.__dict__
         new_dict = {}
         new_dict["__class__"] = type(self).__name__
